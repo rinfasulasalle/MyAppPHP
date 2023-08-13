a37e2b14750e9d890139a6540ba062648e92fff1
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-08-13 00:14:23 -0500

    Agrego el navbar que se usó y el pie de pagina

---
+++ radionet/footer.php
@@ -0,0 +1,5 @@
+<!-- footer.php -->
+<div class="footer text-center py-3 bg-dark text-light">
+    <p>Derechos de Autor © 2023 Roger Infa Sanchezz | Universidad U La Salle</p>
+</div>
+
---
+++ radionet/navbar.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="imgs/logo.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-center">
+            Radio Net
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="radio.php">Radio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="reproductorplaylist.php">Reproductor</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/chat/login.php">Chat</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
